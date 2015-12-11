4e01197f21b36e0d0edfb126fa84d87be2fd2acb
isheejong <isheejong@gmail.com>
2015-12-11 18:08:04 +0900

    ㅗㅗ

--- Rest/src/main/java/jeho/com/filter/GZipRequestWrapper.java
+++ Rest/src/main/java/jeho/com/filter/GZipRequestWrapper.java
@@ -1,47 +1,24 @@
 package jeho.com.filter;
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.zip.GZIPInputStream;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
+import jeho.com.util.GZip;
+
 public class GZipRequestWrapper extends HttpServletRequestWrapper {
-	private byte[] bytes;
-	private GZIPInputStream gzipInputStream;
-	private ByteArrayOutputStream outStream;
+	private byte[] bytes;	
 	
 	public GZipRequestWrapper(HttpServletRequest httpRequest) throws IOException{
 		super(httpRequest);
 		
 		byte[] requestBytes = new byte[1024];
-		try {
-			httpRequest.getInputStream().read(requestBytes);
-			if (requestBytes != null && requestBytes.length > -1) {
-				gzipInputStream = new GZIPInputStream(
-						new BufferedInputStream(new ByteArrayInputStream(requestBytes)));
-				outStream = new ByteArrayOutputStream();
-
-				int size = 0;
-				byte[] buffer = new byte[1024];
-				while ((size = gzipInputStream.read(buffer)) > 0) {
-					outStream.write(buffer, 0, size);
-				}
-				this.bytes = outStream.toByteArray();
-			}
-		} catch (IOException e) {
-			throw e;
-		} finally {
-			if(gzipInputStream != null) {
-				gzipInputStream.close();
-			}			
-			if(outStream != null) {
-				outStream.close();
-			}
+		httpRequest.getInputStream().read(requestBytes);
+		if (requestBytes != null && requestBytes.length > -1) {
+			GZip.decompress(requestBytes);
 		}
 	}
 
---
+++ Rest/src/main/java/jeho/com/util/GZip.java
@@ -0,0 +1,56 @@
+package jeho.com.util;
+
+import java.io.*;
+import java.util.zip.*;
+
+public class GZip {
+
+	public static byte[] compress(String value) throws IOException {
+		byte[] bReturn = null;
+		ByteArrayOutputStream byteArrayOutputStream = null;
+		GZIPOutputStream gzipOutStream = null;
+		try {
+			byteArrayOutputStream = new ByteArrayOutputStream();
+			gzipOutStream = new GZIPOutputStream(new BufferedOutputStream(byteArrayOutputStream));
+			gzipOutStream.write(value.getBytes());
+			bReturn = byteArrayOutputStream.toByteArray();
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			if (gzipOutStream != null) {
+				gzipOutStream.close();
+			}
+			if (byteArrayOutputStream != null) {
+				byteArrayOutputStream.close();
+			}
+		}
+		return bReturn;
+	}
+
+	public static byte[] decompress(byte[] value) throws IOException {
+		byte[] bReturn = null;
+		ByteArrayOutputStream outStream = null;
+		GZIPInputStream gzipInStream = null;
+		try {
+			outStream = new ByteArrayOutputStream();
+			gzipInStream = new GZIPInputStream(new BufferedInputStream(new ByteArrayInputStream(value)));
+			int size = 0;
+			byte[] buffer = new byte[1024];
+			while ((size = gzipInStream.read(buffer)) > 0) {
+				outStream.write(buffer, 0, size);
+			}
+			bReturn = outStream.toByteArray();
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			if (gzipInStream != null) {
+				gzipInStream.close();
+			}
+			if (outStream != null) {
+				outStream.close();
+			}
+		}
+		return bReturn;
+	}
+
+}
