115e12929913ac7c61e61d6868023f0a6d8e822f
mulan101 <mulan101@gmail.com>
2017-08-18 13:16:26 +0900

    엑셀 출력

---
+++ sonarqube/src/main/java/sonarqube/RestClient.java
@@ -0,0 +1,117 @@
+package sonarqube;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+
+public class RestClient {
+	
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
+	public Map<String, Object> restCall(String sUrl, String sComponentRoots, int iPage) {
+		URL url = null;
+		HttpURLConnection conn = null;
+		Map<String, Object> responseBean = null;
+		try {
+			if (sUrl == null || sComponentRoots == null || iPage <= 0) {
+				throw new RuntimeException("Failed : Url/ComponentRoots/TotalPage not found");
+			}
+			log.info("sonarqube server call....");
+			url = new URL(sUrl + "?componentRoots=" + sComponentRoots + "&pageIndex=" + iPage);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			String output = null;
+
+			while ((output = br.readLine()) != null) {
+				responseBean = new Gson().fromJson(output, Map.class);
+			}
+			log.info("sonarqube server call complete....");
+		} catch (MalformedURLException e) {
+			log.error(e.getMessage(), e);
+		} catch (IOException e1) {
+			log.error(e1.getMessage(), e1);
+		} finally {
+			if (conn != null) {
+				conn.disconnect();
+			}
+		}
+		return responseBean;
+	}
+	
+	public void process(String sUrl, String sComponentRoots) {
+		
+		Map<String, Object> getPageMap = restCall(sUrl, sComponentRoots, 1);
+		if (getPageMap == null) {
+			throw new RuntimeException("Error001 : response error");
+		}
+
+		Double iTotal = (Double) getPageMap.get("total");
+		Double iPs = (Double) getPageMap.get("ps");
+		int iLoop = (int) (Math.ceil(iTotal / iPs) + 1);
+		
+		Date d = new Date();
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmss");
+	    
+	    String sProjectName = sComponentRoots.replaceAll(":", "-");
+	        
+		File file = new File(System.getProperty("user.dir") + "\\output\\sonarqube_result_" + sProjectName + "_" + sdf.format(d) + ".xlsx");
+		XSSFExcelWriter writer = new XSSFExcelWriter();
+		writer.createSheet(sProjectName);
+		writer.setColumn(0, "project", "project", 5000);
+		writer.setColumn(1, "severity", "severity", 5000);
+		writer.setColumn(2, "type", "type", 5000);
+		writer.setColumn(3, "rule", "rule", 8000);
+		writer.setColumn(4, "component", "component", 10000);
+		writer.setColumn(5, "line", "line", 1000);
+		writer.setColumn(6, "status", "status", 3000);
+		writer.setColumn(7, "message", "message", 10000);
+		writer.setHeaderToFirstRow();
+		log.info("excel write start...");
+		for(int i = 1 ; i < iLoop ; i++ ) {
+			Map<String, Object> responseMap = restCall(sUrl, sComponentRoots, i);
+			if (responseMap == null) {
+				throw new RuntimeException("Error00" + i + " : response error");
+			}
+			List<Map<String, Object>> issueBeanList = (List<Map<String, Object>>) responseMap.get("issues");
+			if (issueBeanList != null) {
+				writer.setData(issueBeanList);
+			}
+		}
+		
+		writer.setColumnsWidth();
+		writer.write(file);
+		log.info("excel write end...");
+	}
+
+	public static void main(String[] args) {
+		if(args.length != 2) {
+			throw new RuntimeException("Error000 : parameter not found");
+		}		
+		
+		RestClient rc = new RestClient();
+		rc.process(args[0],args[1]);
+
+	}
+
+}
---
+++ sonarqube/src/main/java/sonarqube/XSSFExcelWriter.java
@@ -0,0 +1,226 @@
+package sonarqube;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XSSFExcelWriter {
+	private Logger log = LoggerFactory.getLogger(getClass());
+
+	private XSSFWorkbook workbook;
+
+	private int sheetIndex = 0;
+
+	private List<Column> columns = new ArrayList<Column>();
+
+	private XSSFCellStyle cellStyle;
+
+	private static final String EMPTY_STRING = "";
+
+	public XSSFExcelWriter() {
+		this.workbook = new XSSFWorkbook();
+	}
+
+	public XSSFCellStyle getCellStyle() {
+		if (cellStyle == null)
+			cellStyle = this.createCellStyle();
+		return cellStyle;
+	}
+
+	public void setCellStyle(XSSFCellStyle cellStyle) {
+		this.cellStyle = cellStyle;
+	}
+
+	public XSSFWorkbook getWorkbook() {
+		return workbook;
+	}
+
+	public int getSheetIndex() {
+		return sheetIndex;
+	}
+
+	public void setSheetIndex(int sheetIndex) {
+		this.sheetIndex = sheetIndex;
+	}
+
+	public void setWorkbook(XSSFWorkbook workbook) {
+		this.workbook = workbook;
+	}
+
+	public void createSheet(String name) {
+		XSSFSheet sheet = this.workbook.createSheet(name);
+		this.sheetIndex = workbook.getSheetIndex(sheet);
+	}
+
+	public XSSFRow addRow(int rownum) {
+		XSSFSheet sheet = getSheetAt(getSheetIndex());
+		return sheet.createRow(rownum);
+	}
+
+	public XSSFCell addCell(int rownum, int column, XSSFCellStyle cellStyle) {
+		XSSFCell cell = getRow(rownum).createCell(column);
+		if (cellStyle != null)
+			cell.setCellStyle(cellStyle);
+
+		return cell;
+	}
+
+	public XSSFSheet getSheetAt(int sheetIndex) {
+		return workbook.getSheetAt(sheetIndex);
+	}
+
+	public int getFirstRowNum() {
+		return getSheetAt(getSheetIndex()).getFirstRowNum();
+	}
+
+	public XSSFRow getRow(int rownum) {
+		return getSheetAt(getSheetIndex()).getRow(rownum);
+	}
+
+	private XSSFCellStyle createCellStyle() {
+		XSSFCellStyle style = workbook.createCellStyle();
+		style.setVerticalAlignment(VerticalAlignment.CENTER);
+		style.setBorderBottom(BorderStyle.THIN);
+		style.setBottomBorderColor(IndexedColors.BLACK.index);
+		style.setBorderLeft(BorderStyle.THIN);
+		style.setLeftBorderColor(IndexedColors.GREEN.index);
+		style.setBorderRight(BorderStyle.THIN);
+		style.setRightBorderColor(IndexedColors.BLUE.index);
+		style.setBorderTop(BorderStyle.THIN);
+		style.setTopBorderColor(IndexedColors.BLACK.index);
+		return style;
+	}
+
+	public void setHeaderToFirstRow() {
+		int rowNum = getFirstRowNum();
+		XSSFRow row = addRow(rowNum);
+		XSSFCellStyle cellStyle = createCellStyle();
+		cellStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.index);
+		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		cellStyle.setAlignment(HorizontalAlignment.CENTER_SELECTION);
+		cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+		XSSFFont font = workbook.createFont();
+		font.setBold(true);
+		cellStyle.setFont(font);
+		for (Column c : columns) {
+			XSSFCell cell = getRow(row.getRowNum()).createCell(c.columnIndex);
+			cell.setCellStyle(cellStyle);
+			cell.setCellValue(c.name);
+		}
+	}
+
+	public int getLastRowNum() {
+		return getSheetAt(getSheetIndex()).getLastRowNum();
+	}
+
+	public void setData(List<Map<String, Object>> items) {
+		if (items != null) {
+			for (Map<String, Object> item : items) {
+				setDataToRow(item);
+			}
+		}
+	}
+
+	public void setDataToRow(Map<String, Object> data) {
+		XSSFRow row = addRow(getLastRowNum() + 1);
+		for (Column c : columns) {
+			XSSFCell cell = addCell(row.getRowNum(), c.columnIndex, getCellStyle());
+			Object value = data.get(c.valueKey);
+			cell.setCellType(CellType.STRING);
+			if (value != null)
+				cell.setCellValue(value.toString().trim());
+			else
+				cell.setCellValue(EMPTY_STRING);
+		}
+	}
+
+	public void write(File file) {
+		FileOutputStream fs = null;
+		try {
+			fs = new FileOutputStream(file);
+			workbook.write(fs);
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		} finally {
+			if (fs != null)
+				try {
+					fs.close();
+				} catch (IOException e) {
+					log.error(e.getMessage(), e);
+				}
+		}
+	}
+
+	public void write(OutputStream output) {
+		try {
+			workbook.write(output);
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		} finally {
+			if (output != null)
+				try {
+					output.close();
+				} catch (IOException e) {
+					log.error(e.getMessage(), e);
+				}
+		}
+	}
+
+	public void setColumn(int idx, String name, String dataKey) {
+		this.columns.add(new Column(idx, name, dataKey));
+	}
+
+	public void setColumn(int idx, String name, String dataKey, int width) {
+		this.columns.add(new Column(idx, name, dataKey, width));
+	}
+
+	public void setColumnsWidth() {
+		for (Column c : columns) {
+			if (c.width > 0) {
+				getSheetAt(getSheetIndex()).setColumnWidth(c.columnIndex, c.width);
+			}
+		}
+	}
+
+	public static class Column {
+		String name;
+		String valueKey;
+		int columnIndex;
+		int width = 0;
+
+		public Column(int columnIndex, String name, String valueKey) {
+			super();
+			this.columnIndex = columnIndex;
+			this.name = name;
+			this.valueKey = valueKey;
+		}
+
+		public Column(int columnIndex, String name, String valueKey, int width) {
+			super();
+			this.columnIndex = columnIndex;
+			this.name = name;
+			this.valueKey = valueKey;
+			this.width = width;
+		}
+
+	}
+}
